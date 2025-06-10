38686903b36e2133b827ddcb431e0661491eb808
krishprasad63 <krishprasad.kp7@gmail.com>
2025-06-10 22:21:19 +0530

    verilog experiments

--- 2_One_Bit_Full_Adder_using_Half_Adder/full_adder_tb.v
+++ 2_One_Bit_Full_Adder_using_Half_Adder/full_adder_tb.v
@@ -27,7 +27,6 @@ module full_adder_using_half_adder_tb;
         #10 a = 0; b = 0; cin = 1;
         #10 a = 0; b = 1; cin = 1;
         #10 a = 1; b = 0; cin = 1;
-        #10 a = 1; b = 1; cin = 1;
         #10 $finish;
     end
     
