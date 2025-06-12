44c93319cde64c81e578e3f574f179361e81924b
krishprasad63 <krishprasad.kp7@gmail.com>
2025-06-12 13:25:06 +0530

    verilog experiments

--- 0001001_3_to_8_Decoder_using_case_statement/3_to_8_decoder.v
+++ 0001001_3_to_8_Decoder_using_case_statement/3_to_8_decoder.v
@@ -7,7 +7,16 @@ module three_to_eight_decoder(
     output reg [7:0] out
 );
     always @(*) begin
-        out = 8'b00000000;
-        out[inp] = 1'b1;
+        case (inp)
+            3'b000: out = 8'b00000001;
+            3'b001: out = 8'b00000010;
+            3'b010: out = 8'b00000100;
+            3'b011: out = 8'b00001000;
+            3'b100: out = 8'b00010000;
+            3'b101: out = 8'b00100000;
+            3'b110: out = 8'b01000000;
+            3'b111: out = 8'b10000000;
+            default: out = 8'b00000000; // Should never happen with 3-bit input
+        endcase
     end
 endmodule
